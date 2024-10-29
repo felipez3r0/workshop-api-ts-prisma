702e76678460b4164723d14b5aae200a062c2cb3
Felipe Do E. Santo <felipez3r0@gmail.com>
2024-10-29 21:58:01 +0000

    feat: adiciona swagger e documentação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express' // Importa o Express
 import routes from './routes' // Importa as rotas
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json' // Carregar o arquivo JSON
 
 const app = express() // Cria uma instância do Express
 const PORT = 3000 // Define a porta do servidor
 app.use(express.json()) // Habilita o uso de JSON nas requisições
 
 app.use('/api', routes) // Define o prefixo para as rotas
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile)) // Configura o Swagger UI
 
 app.listen(PORT, () => {
   console.log(`Server rodando em http://localhost:${PORT}`) // Inicia o servidor
